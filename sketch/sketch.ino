c40c55780abd11531beeab63bca45c6c57cb37f5
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-03-29 16:11:55 +0200

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,45 +1,36 @@
 #include <Arduino.h>
 #include <Arduino_RouterBridge.h>
 
-static void setChannel(uint8_t pin, bool logicalOn) {
+void setChannel(uint8_t pin, bool logicalOn) {
   // active-low
   digitalWrite(pin, logicalOn ? LOW : HIGH);
 }
 
-static void applyLed(const char* ledName, bool ledOn, bool r, bool g, bool b) {
-  uint8_t pinR = 0;
-  uint8_t pinG = 0;
-  uint8_t pinB = 0;
 
-  if (strcmp(ledName, "led3") == 0) {
-    pinR = LED3_R;
-    pinG = LED3_G;
-    pinB = LED3_B;
-  } else if (strcmp(ledName, "led4") == 0) {
-    pinR = LED4_R;
-    pinG = LED4_G;
-    pinB = LED4_B;
+void onSetMcuLed( String ledName, int onValue, int rValue, int gValue, int bValue) {
+ 
+   uint8_t pinR; 
+   uint8_t pinG;
+   uint8_t pinB;
+  
+  if (ledName == "led3") {
+     pinR = LED3_R;
+     pinG = LED3_G;
+     pinB = LED3_B;
+  } else if (ledName == "led4") {
+     pinR = LED4_R;
+     pinG = LED4_G;
+     pinB = LED4_B;
   } else {
     return;
   }
 
-  setChannel(pinR, ledOn && r);
-  setChannel(pinG, ledOn && g);
-  setChannel(pinB, ledOn && b);
-}
-
-void onSetMcuLed(String ledName, int onValue, int rValue, int gValue, int bValue) {
-  bool ledOn = (onValue != 0);
-  bool r = (rValue != 0);
-  bool g = (gValue != 0);
-  bool b = (bValue != 0);
-
-  applyLed(ledName.c_str(), ledOn, r, g, b);
+  setChannel(pinR, onValue && rValue);
+  setChannel(pinG, onValue && gValue);
+  setChannel(pinB, onValue && bValue);
 }
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
 
   pinMode(LED3_R, OUTPUT);
   pinMode(LED3_G, OUTPUT);
@@ -57,12 +48,9 @@ void setup() {
 
   Bridge.begin();
   Bridge.provide("set_mcu_led", onSetMcuLed);
-
-  Serial.println("MCU RGB controller ready");
+  Monitor.begin();
+  delay(1000);
+  Monitor.println("MCU RGB controller ready");
 }
 
-void loop() {
-  
-}
-
-
+void loop() {}
